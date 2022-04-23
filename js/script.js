b29fe5ff24809bd8cd5d10c81c3e176fd88eb3c0
volkovmisha <88mizrael@gmail.com>
2022-04-23 18:03:25 +0300

    homework9

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
 const inputNameE = document.getElementById('name')
+const lastNameE = document.getElementById('lastname')
+const phoneE = document.getElementById('phone')
 const btnE = document.getElementById('addToDo')
 const todoFormE = document.getElementById('todoForm')
 const todoList = document.getElementById('todoList')
+const todoFormInputsE = document.querySelectorAll('input')
+const rowTemplateE = document.getElementById('rowTemplate')
+
 todoFormE.addEventListener('keyup',changeTodoForm)
 btnE.addEventListener('click', onAddButtonClick)
 
-function changeTodoForm(e) {
-
-    e.target.value.length ? btnE.disabled = false : btnE.disabled = true;
+function changeTodoForm() {
+    isFieldsNotEmpty()
 }
 function onAddButtonClick() {
-    const listElementValue = inputNameE.value;
-    const listElement = document.createElement('li')
-    listElement.textContent = listElementValue;
-    todoList.append(listElement)
+    const toDoRow = createToDoRow()
+    addToDoRow(toDoRow)
     clearForm()
+}
+
+function createToDoRow() {
+    return rowTemplateE.innerHTML
+        .replace("{{name}}",inputNameE.value)
+        .replace("{{lastName}}",lastNameE.value)
+        .replace("{{phone}}",phoneE.value)
+}
+
+function addToDoRow(toDoRow) {
+    todoList.innerHTML += toDoRow
 }
 
 function clearForm() {
     todoFormE.reset()
     btnE.disabled = true;
+}
+
+function isFieldsNotEmpty() {
+    let isDisabled = false;
+    todoFormInputsE.forEach(i => {
+        if(!i.value.length) {
+            isDisabled = true;
+        }
+    })
+    isDisabled ? btnE.disabled = true : btnE.disabled = false;
 }
