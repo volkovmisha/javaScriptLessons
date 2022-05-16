f7516583b6e303fb8c1c1f2bc829a2344aabc17e
volkovmisha <88mizrael@gmail.com>
2022-05-16 10:35:48 +0300

    hm15

--- js/script.js
+++ js/script.js
@@ -1,38 +1,15 @@
-class Tab {
-    children = []
-    static CLASSES = {
-        title: 'tab-title',
-        body: 'tab-body'
-    }
-    constructor(element) {
-        this.children = [...element.children]
-        this.addChildrenClasses()
-        this.children.forEach(e => console.log(e))
-    }
+const menu = document.getElementById('menu')
+window.addEventListener('resize', resize);
 
-    addChildrenClasses() {
-        this.children.forEach(e => {
-            const [title, body] = e.children
-            title.classList.add(Tab.CLASSES.title)
-            body.classList.add(Tab.CLASSES.body)
-            /* кстати насколько легально вообще задавать слушатели напрямую, вот так? */
-            title.addEventListener('click', this.changeTab)
-            title.addEventListener('mouseover', this.hoverTab)
-            title.addEventListener('mouseout', this.unHoverTab)
-        })
-    }
-    changeTab(e) {
-        const prevClicks = document.querySelectorAll('.selected')
-        prevClicks.length ? prevClicks[0].classList.remove('selected') : '';
-        e.target.classList.add('selected')
-    }
-    hoverTab(e) {
-        e.target.classList.add('hover')
-    }
-    unHoverTab(e) {
-        e.target.classList.remove('hover')
+const MOBILE_WIDTH = 768;
+resize();
+
+function resize() {
+    const windowWidth = window.innerWidth
+    if(windowWidth > MOBILE_WIDTH) {
+        menu.classList.remove('show')
+    } else {
+        menu.classList.add('show')
+
     }
 }
-
-const targetElement = document.querySelector('.tabs')
-const tab = new Tab(targetElement)
