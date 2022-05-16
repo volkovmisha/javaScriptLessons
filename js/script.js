91291388993c5e0b581a5db9aa62ba2371902285
volkovmisha <88mizrael@gmail.com>
2022-05-16 11:06:20 +0300

    hm13

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 class Group {
-
-    constructor() {
-         this.studentsList = [];
-    }
+    #studentsList = []
     addStudent(student) {
-        this.studentsList.push(student);
-    }
-    static getStudent() {
-        return this.studentsList;
+        this.#studentsList.push(student);
     }
 
     getAverageMark() {
@@ -18,6 +12,10 @@ class Group {
             },0)) / student.marks.length
             return acc
         },0) / this.studentsList.length)
+    }
+
+    get studentsList(){
+        return this.#studentsList;
     }
 }
 
@@ -33,17 +31,10 @@ const jhon2 = new Student('jhon2',[5,6,8,1])
 const jhon3 = new Student('jhon3',[5,5,5,3])
 const jhon4 = new Student('jhon',[10,10,4,3])
 
-console.log(jhon)
-console.log(jhon2)
-console.log(jhon3)
-console.log(jhon4)
-
 const group1 = new Group()
 group1.addStudent(jhon)
 group1.addStudent(jhon2)
 group1.addStudent(new Student('IVAN',[1,0,0,2]))
 
-
 console.log(group1)
 console.log(group1.getAverageMark())
-console.log(group1.getStudent())
